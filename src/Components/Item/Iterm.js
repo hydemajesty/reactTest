c0ea7c6f0cf746472ed317eb2ac73f40d00a98eb
yi.cheng <yi.cheng@capitaledge.cn>
2022-02-14 18:01:44 +0800

    #1
    item内容，高亮显示选中及删除按钮

--- src/Components/Item/Iterm.js
+++ src/Components/Item/Iterm.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react'
 import './Item.css'
 
 export default class Iterm extends Component {
+  
+  state = {mouse:false}
+  handleMouse = (flag) => {
+    return ()=>{
+      this.setState({mouse:flag})
+    }
+  }
+  
+
   render() {
     const {name,done} = this.props
+    const {mouse} = this.state
     return (
-      <li>
+      <li style={{backgroundColor:mouse?'#ddd':'white'}} onMouseEnter={this.handleMouse(true) } onMouseLeave={this.handleMouse(false)}>
 				<label>
 					<input type="checkbox"/>
 					<span>{name}</span>
 				</label>
-				<button className="btn btn-danger" style={{display:'none'}}>删除</button>
+				<button className="btn btn-danger" style={{display:mouse?'block':'none'}}>删除</button>
 			</li>
     )
   }
